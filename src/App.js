3cd773e6463d10066b6c08297efda08f88c9f7f4
Jhonni Calle <jhonni.calle@xyclon.com.ar>
2021-05-17 14:39:38 -0400

    Testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import MetaTags from 'react-meta-tags';
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
     <>
       <MetaTags>
-        <title>Page 1</title>
+        {/* <title>Page 1</title> */}
         <meta name="description" content="Some description." />
+        <meta property="og:description" content="Some description." />
         <meta property="og:title" content="MyApp" />
         <meta property="og:image" content="src/images/sumate_a_nuestro_equipo.jpg" />
       </MetaTags>
+      {/* <Helmet>
+            <meta charSet="utf-8" />
+            <title>My Title</title>
+            <meta name="description" content="Helmet application" />
+            <link rel="canonical" href="http://mysite.com/example" />
+        </Helmet> */}
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
